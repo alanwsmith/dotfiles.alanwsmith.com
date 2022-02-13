6a10c1faa6b0140f92940aba4da829941f15f0e8
Alan Smith <alan@alanwsmith.com>
2022-02-13 17:03:07 -0500

    Setup sleep timer as variable and put in third attempt to move app

--- dotfiles/yabai/resize-scripts/4k-main.py
+++ dotfiles/yabai/resize-scripts/4k-main.py
@@ -6,6 +6,9 @@ import sys
 import time
 
 class AppMover():
+    def __init__(self):
+        self.time_padding = 0.2
+
     def app_space_id(self, app):
         for window in self.windows():
             if window['app'] == app:
@@ -37,7 +40,7 @@ class AppMover():
             print(f"Opening {app}")
             subprocess.run(['open', '-a', app])
             for i in range(1,50):
-                time.sleep(.1)
+                time.sleep(self.time_padding)
                 print('.', end='')
                 if self.window_id_for_app(app) != None:
                     print(f"Opened {app}")
@@ -67,58 +70,73 @@ class AppMover():
         subprocess.run(f"/opt/homebrew/bin/yabai -m window --resize top:0:-{amount}".split(' '), check=True)
 
     def focus_app(self, app):
+        print(f"Switching focus to {app}")
+        # See if it's already in focus
+        for window in self.windows():
+            if window['app'] == app and window['has-focus']:
+                print(f"{app} already has focus")
+                return True
+        # otherwise make sure it's open and switch focus to it
         self.ensure_app_is_open(app)
         print(f"Focusing: {app}")
         for window in self.windows():
             if window['app'] == app:
                 subprocess.run(f"/opt/homebrew/bin/yabai -m window --focus {window['id']}".split(' '), check=True)
-                for i in range(1,20):
+                for i in range(1,14):
                     print(f"Checking focus on: {app}")
                     for check_window in self.windows():
                         if check_window['has-focus'] == True and check_window['app'] == app:
                             print(f"Confirmed {app} is in focus")
-                            time.sleep(0.1)
+                            # need this padding to let the system catch up
+                            time.sleep(self.time_padding)
                             return True
-                    time.sleep(0.1)
-        print(f"Could not focus on {app}") 
+                    time.sleep(self.time_padding)
+        print(f"Could not focus on {app}")
         print("Process halted")
         sys.exit()
 
     def insert_from_anchor(self, anchor_app, direction, new_app):
         print(f"Insert from anchor: {anchor_app} - {direction} - {new_app}")
         self.focus_app(anchor_app)
-        #time.sleep(0.3)
         subprocess.run(f"/opt/homebrew/bin/yabai -m window --insert {direction}".split(' '), check=True)
-        # Not sure of a good way to check that this is set, so just adding some time
-        #time.sleep(0.3)
         self.move_app_to_space(new_app, self.app_space_id(anchor_app))
 
-
     def move_app_to_space(self, app, space):
-        # TODO: Check if the app is already on the space and don't mess with it if it is
         print(f"Moving: {app} to: {space}")
         if self.app_space_id(app) == space:
             print(f"No need to move. {app} is already on space {space}")
         else:
+            # TODO: Make this a recursive function instead of copy paste    
             self.focus_app(app)
             subprocess.run(f"/opt/homebrew/bin/yabai -m window --space {space}".split(' '), check=True)
-            print(f"Confirming move.")
-            for i in range(1, 35):
+            print(f"Confirming {app} moved to space {space}")
+            for i in range(1, 20):
                 if self.app_space_id(app) == space:
-                    print(f"Moved: {app} to: {space}")
+                    print(f"- Moved: {app} to: {space}")
                     return True
-                time.sleep(0.1)
+                time.sleep(self.time_padding)
                 print('.', end='')
-            print("Move didn't work. Trying again")
+
+            print("- Move didn't work. Trying again")
             self.focus_app(app)
             subprocess.run(f"/opt/homebrew/bin/yabai -m window --space {space}".split(' '), check=True)
-            print(f"Confirming move.")
-            for i in range(1, 40):
+            for i in range(1, 30):
                 if self.app_space_id(app) == space:
-                    print(f"Moved: {app} to: {space}")
+                    print(f"- Moved: {app} to: {space}")
                     return True
-                time.sleep(0.1)
+                time.sleep(self.time_padding)
                 print('.', end='')
+
+            print("- Move didn't work. Trying again")
+            self.focus_app(app)
+            subprocess.run(f"/opt/homebrew/bin/yabai -m window --space {space}".split(' '), check=True)
+            for i in range(1, 30):
+                if self.app_space_id(app) == space:
+                    print(f"- Moved: {app} to: {space}")
+                    return True
+                time.sleep(self.time_padding)
+                print('.', end='')
+
             print()
             print(f"Failed to move {app} to space {space}")
             print(f"Process halted")
@@ -160,11 +178,12 @@ class AppMover():
 
 if __name__ == "__main__":
     am = AppMover()
+    am.time_padding = 0.2
 
     am.stage_apps()
 
     am.move_app_to_space('Adobe Photoshop 2022', 2)
-    am.move_app_to_space('Safari', 5)
+    am.move_app_to_space('Code', 5)
     am.move_app_to_space('Discord', 6)
     am.move_app_to_space('Music', 7)
     am.move_app_to_space('1Password 7', 8)
@@ -172,7 +191,7 @@ if __name__ == "__main__":
 
     am.move_app_to_space('iTerm2', 1)
     am.insert_from_anchor('iTerm2', 'west', 'Google Chrome')
-    am.expand_left('iTerm2', 530)
+    am.expand_left('iTerm2', 570)
     am.insert_from_anchor('iTerm2', 'north', 'GitHub Desktop')
     am.expand_top('iTerm2', 390)
     am.insert_from_anchor('iTerm2', 'south', 'CodeRunner')
@@ -180,7 +199,10 @@ if __name__ == "__main__":
     am.insert_from_anchor('CodeRunner', 'east', 'Sublime Text')
     am.insert_from_anchor('Google Chrome', 'south', 'nvALT')
     am.insert_from_anchor('iTerm2', 'east', 'Terminal')
-    am.expand_right('iTerm2', 280)
+    am.expand_right('iTerm2', 300)
+    am.insert_from_anchor('GitHub Desktop', 'east', 'Safari')
+    am.expand_right('GitHub Desktop', 140)
 
+    am.focus_app('Music')
     am.focus_app('iTerm2')
 
