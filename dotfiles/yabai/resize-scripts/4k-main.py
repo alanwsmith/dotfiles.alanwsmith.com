1d74290d857e175b500db5eb18c0bb0cb6cafbb3
Alan Smith <alan@alanwsmith.com>
2022-02-13 01:11:33 -0500

    Updated README

--- dotfiles/yabai/resize-scripts/4k-main.py
+++ dotfiles/yabai/resize-scripts/4k-main.py
@@ -158,12 +158,16 @@ class AppMover():
 
 if __name__ == "__main__":
     am = AppMover()
-    am.stage_apps()
 
-    am.move_app_to_space('Adobe Photoshop 2022', 2)
-    am.move_app_to_space('Safari', 5)
-    am.move_app_to_space('Discord', 6)
-    am.move_app_to_space('Music', 7)
+    # am.stage_apps()
+    # am.move_app_to_space('iTerm2', 6)
+
+    # am.move_app_to_space('Adobe Photoshop 2022', 2)
+    # am.move_app_to_space('Safari', 5)
+    # am.move_app_to_space('Discord', 6)
+    # am.move_app_to_space('Music', 7)
+    # am.move_app_to_space('1Password 7', 8)
+    # am.move_app_to_space('Keychain Access', 9)
 
     am.move_app_to_space('iTerm2', 1)
     am.insert_from_anchor('iTerm2', 'west', 'Google Chrome')
@@ -176,6 +180,5 @@ if __name__ == "__main__":
     am.insert_from_anchor('Google Chrome', 'south', 'nvALT')
     am.insert_from_anchor('iTerm2', 'east', 'Terminal')
     am.expand_right('iTerm2', 300)
-
     am.focus_app('iTerm2')
 
