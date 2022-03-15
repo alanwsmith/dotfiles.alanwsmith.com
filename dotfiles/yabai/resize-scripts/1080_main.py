05cc7d1672686e5ed9224a3766c7527b64015610
Alan Smith <alan@alanwsmith.com>
2022-03-14 20:22:37 -0400

    Updated scripts

--- dotfiles/yabai/resize-scripts/1080_main.py
+++ dotfiles/yabai/resize-scripts/1080_main.py
@@ -4,35 +4,35 @@ import json
 import subprocess
 import sys
 import time
+from app_mover import AppMover
 
 if __name__ == "__main__":
     am = AppMover()
 
     am.stage_apps()
-    am.move_app_to_space('GitHub Desktop', 2)
-    am.move_app_to_space('Adobe Photoshop 2022', 3)
-    am.move_app_to_space('DB Browser for SQLite', 4)
-    am.move_app_to_space('Code', 5)
-    am.move_app_to_space('Discord', 6)
-    am.move_app_to_space('Music', 7)
-    am.move_app_to_space('1Password 7', 8)
-    # am.move_app_to_space('Keychain Access', 9)
+
+    # Screen 1 - Desktops 1-8
 
     am.move_app_to_space('iTerm2', 1)
     am.insert_from_anchor('iTerm2', 'west', 'Google Chrome')
-    # am.expand_left('iTerm2', 280)
     am.insert_from_anchor('Google Chrome', 'south', 'nvALT')
+    am.expand_left('iTerm2', 220)
 
-    # am.insert_from_anchor('iTerm2', 'north', 'GitHub Desktop')
-    # am.expand_top('iTerm2', 390)
-    # am.insert_from_anchor('iTerm2', 'south', 'CodeRunner')
-    # am.expand_bottom('iTerm2', 130)
-    # am.insert_from_anchor('CodeRunner', 'east', 'Sublime Text')
-    # am.insert_from_anchor('iTerm2', 'east', 'Terminal')
-    # am.expand_right('iTerm2', 300)
-    # am.insert_from_anchor('GitHub Desktop', 'east', 'Safari')
-    # am.expand_right('GitHub Desktop', 140)
+    am.move_app_to_space('GitHub Desktop', 2)
 
-    # am.focus_app('Music')
+    am.move_app_to_space('Code', 3)
+
+    am.move_app_to_space('CodeRunner', 4)
+    am.insert_from_anchor('CodeRunner', 'east', 'Sublime Text')
+
+    am.move_app_to_space('Adobe Photoshop 2022', 4)
+
+    # Screen 2 - Desktops 9-16
+    am.move_app_to_space('Safari', 9)
+    am.move_app_to_space('Discord', 10)
+    am.move_app_to_space('1Password 7', 11)
+    am.insert_from_anchor('1Password 7', 'south', 'Keychain Access')
+
+    am.focus_app('Safari')
     am.focus_app('iTerm2')
 
