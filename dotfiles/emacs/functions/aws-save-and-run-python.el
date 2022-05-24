aa934b7865e47b993d100e6bfceca3a774a6a297
Alan Smith <commit@alanwsmith.com>
2022-05-24 15:03:10 -0400

    Create aws-save-and-run-python.el

---
+++ dotfiles/emacs/functions/aws-save-and-run-python.el
@@ -0,0 +1,20 @@
+(defun aws-save-and-run-python (arg)
+  "Copy of spacemacs/python-execute-file that saves
+the file before compiling and executing. Original source
+is: https://github.com/syl20bnr/spacemacs/blob/b3e67aafe2451ca91e2d310d29879616e10981d0/layers/%2Blang/python/funcs.el#L519"
+
+  (interactive "P")
+  (save-buffer)
+  (let ((universal-argument t)
+        (compile-command
+         (format
+          "%s %s"
+          (spacemacs/pyenv-executable-find python-shell-interpreter)
+          (shell-quote-argument
+           (file-name-nondirectory buffer-file-name)))))
+    (if arg
+        (call-interactively 'compile)
+      (compile compile-command t)
+      (with-current-buffer (get-buffer "*compilation*")
+        (inferior-python-mode)))))
+)
