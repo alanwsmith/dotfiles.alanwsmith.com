e779f3dfed3c61e9c74427f5cbf81168035858bc
Alan Smith <commit@alanwsmith.com>
2022-05-28 23:28:55 -0400

    Script to run python files with large output in emacs that sends to vscode

---
+++ dotfiles/emacs/functions/run-python.el
@@ -0,0 +1,38 @@
+;; Remember that this file sticks arround until it gets cleaned
+;; up by what ever sweeps the tmp directory so beware of
+;; using it for sensitive data without cleaning it.
+
+(defvar aws-run-python-tmp-file "/tmp/aws-run-python-tmp")
+
+(defun aws-run-python ()
+  (interactive)
+
+  (save-buffer)
+
+  (call-process
+   "/bin/bash"
+   nil
+   nil
+   nil
+   "-c"
+   (concat
+   "/opt/homebrew/bin/python3"
+   " "
+   (buffer-file-name)
+   " "
+   ">"
+   " "
+   aws-run-python-tmp-file
+   )
+   )
+
+   (call-process
+   "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+   ;; "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+    nil
+    nil
+    nil
+    aws-run-python-tmp-file
+    )
+
+   )
