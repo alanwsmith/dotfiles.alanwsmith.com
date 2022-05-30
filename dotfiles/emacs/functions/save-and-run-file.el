ab19e79c1b253062fbee0ffd4ea4e48f2ad1cfc0
Alan Smith <commit@alanwsmith.com>
2022-05-30 13:14:15 -0400

    Added file to run a buffer directly and send it's output to a tmp file to open in another app

---
+++ dotfiles/emacs/functions/save-and-run-file.el
@@ -0,0 +1,36 @@
+;; The goal here is to make it easy to hit a single
+;; not key to run files
+
+;; Tried to setup a varaible for the log file, but couldn't
+;; get it to work. See:
+;; https://emacs.stackexchange.com/q/71942/38139
+
+
+(defvar save-and-run-output-app
+  "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" 
+  "The application to use to open the
+output file"
+  )
+
+
+(defvar save-and-run-output-path
+  "/Users/alan/Desktop/emacs-log.txt"
+  "Output location for the tmp file to use"
+  )
+
+(defun save-and-run-file (arg) "Save and run a file"
+       (interactive "P")
+       (save-buffer)
+       (call-process (shell-quote-argument buffer-file-name) nil `(:file ,save-and-run-output-path))
+       (call-process save-and-run-output-app nil nil nil "/Users/alan/Desktop/emacs-log.txt")
+       (call-process "osascript" nil nil nil "-l" "JavaScript" "-e" "var emacs = new Application('Emacs'); emacs.activate()")
+       )
+
+(add-hook 'python-mode-hook
+          (lambda () (local-set-key (kbd "s-r") 'save-and-run-file))
+          )
+
+(add-hook 'js2-mode-hook
+          (lambda () (local-set-key (kbd "s-r") 'save-and-run-file))
+          )
+
