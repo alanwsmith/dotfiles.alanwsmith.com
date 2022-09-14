517750af7968457aa87a9a2d5296e86ede45269d
Alan Smith <commit@alanwsmith.com>
2022-09-14 00:34:30 -0400

    Updates

--- dotfiles/cli-shortcuts/cdkinit.zsh
+++ dotfiles/cli-shortcuts/cdkinit.zsh
@@ -10,6 +10,8 @@ function cdkinit () {
   python -m pip install -r requirements.txt && \
   echo "venv" >> .gitignore && \
   source venv/bin/activate && \
+  pip install aws-cdk.aws-apigatewayv2-alpha && \
+  pip install aws-cdk.aws-apigatewayv2-integrations-alpha && \
   rm source.bat && \
   rm README.md && \
   touch README.md
