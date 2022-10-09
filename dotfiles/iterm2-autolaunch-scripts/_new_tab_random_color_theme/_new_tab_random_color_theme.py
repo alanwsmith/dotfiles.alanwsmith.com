292e2ad3db9dc5ac4cc819393825a37b8c7716fa
Alan Smith <commit@alanwsmith.com>
2022-10-09 00:51:37 -0400

    Printing out the selected theme

--- dotfiles/iterm2-autolaunch-scripts/_new_tab_random_color_theme/_new_tab_random_color_theme.py
+++ dotfiles/iterm2-autolaunch-scripts/_new_tab_random_color_theme/_new_tab_random_color_theme.py
@@ -3,6 +3,13 @@
 import iterm2
 import random
 
+# This is a copy of this script:
+# https://iterm2.com/python-api/examples/random_color.html
+
+# TODO: Filter out the light mode presets that 
+# can't be removed
+
+
 async def SetPresetInSession(connection, session, preset_name):
     preset = await iterm2.ColorPreset.async_get(connection, preset_name)
     if not preset:
@@ -15,12 +22,13 @@ async def SetPresetInSession(connection, session, preset_name):
 async def main(connection):
     app = await iterm2.async_get_app(connection)
     color_preset_names = await iterm2.ColorPreset.async_get_list(connection)
-    print(color_preset_names)
     async with iterm2.NewSessionMonitor(connection) as mon:
         while True:
             session_id = await mon.async_get()
             session = app.get_session_by_id(session_id)
             if session:
-                await SetPresetInSession(connection, session, random.choice(color_preset_names))
+                new_theme = random.choice(color_preset_names)
+                print(f"Switching to theme: {new_theme}")
+                await SetPresetInSession(connection, session, new_theme)
 
 iterm2.run_forever(main)
